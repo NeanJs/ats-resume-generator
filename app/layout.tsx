c0e46d066713a108203fed0052d384d1f7472fad
Neanjs <neancodes@gmail.com>
2026-06-10 09:43:09 -0700

    deployment v2.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Geist } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
-import Navbar from "../components/Navbar";
 
 const geist = Geist({
   variable: "--font-geist-sans",
